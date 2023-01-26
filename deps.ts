b8f44102d6adb982e35105e617f6635c354c6eee
tenkawa <tenriver.ocean0@gmail.com>
2023-01-26 15:18:41 +0000

    kwik修正

--- deps.ts
+++ deps.ts
@@ -5,4 +5,4 @@ export { config as dotEnvConfig } from "https://deno.land/std@0.167.0/dotenv/mod
 export * from "https://deno.land/x/discordeno@13.0.0-rc31/mod.ts";
 export * from "https://deno.land/x/discordeno@13.0.0-rc31/plugins/mod.ts";
 // Database, thx Tri!
-export * from "https://deno.land/x/kwik@v1.2.1/mod.ts";
+export * from "https://deno.land/x/kwik@v1.3.1/mod.ts";
