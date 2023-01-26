5ff98720e0e21ee4fdf40b1d3465c7eb767f629a
tenkawa <tenriver.ocean0@gmail.com>
2023-01-26 15:43:01 +0000

    realPathSyncを使用しないように修正

--- mod.ts
+++ mod.ts
@@ -7,13 +7,9 @@ import { updateApplicationCommands } from "./src/utils/updateCommands.ts";
 log.info("Starting bot...");
 
 // Forces deno to read all the files which will fill the commands/inhibitors cache etc.
-await Promise.all(
-  [
-    "./src/commands",
-    "./src/events",
-    // "./src/tasks",
-  ].map((path) => importDirectory(Deno.realPathSync(path))),
-);
+importDirectory(await Deno.realPath("./src/commands"));
+importDirectory(await Deno.realPath("./src/events"));
+
 await fileLoader();
 
 // UPDATES YOUR COMMANDS TO LATEST COMMANDS
