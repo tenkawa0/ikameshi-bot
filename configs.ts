3be8edc5a1f8510dd45be1ad3368b4a5c312af82
tenkawa <tenriver.ocean0@gmail.com>
2023-01-26 16:05:00 +0000

    revert

--- configs.ts
+++ configs.ts
@@ -1,10 +1,8 @@
 import { dotEnvConfig } from "./deps.ts";
 
 // Get the .env file that the user should have created, and get the token
-await dotEnvConfig({ export: true });
-const config = Deno.env.toObject();
-
-const token = config["BOT_TOKEN"] || "";
+const env = dotEnvConfig({ export: true });
+const token = env.BOT_TOKEN || "";
 
 export interface Config {
   token: string;
@@ -17,5 +15,5 @@ export const configs = {
   /** Get the BotId from the token */
   botId: BigInt(atob(token.split(".")[0])),
   /** The server id where you develop your bot and want dev commands created. */
-  devGuildId: BigInt(config["DEV_GUILD_ID"]!),
+  devGuildId: BigInt(env.DEV_GUILD_ID!),
 };
--- mod.ts
+++ mod.ts
@@ -1,15 +1,21 @@
-import { Bot } from "./bot.ts";
 import { startBot } from "./deps.ts";
-import { fileLoader, importDirectory } from "./src/utils/loader.ts";
 import log from "./src/utils/logger.ts";
+import { fileLoader, importDirectory } from "./src/utils/loader.ts";
 import { updateApplicationCommands } from "./src/utils/updateCommands.ts";
+// setup db
+import "./src/database/mod.ts";
+import { Bot } from "./bot.ts";
 
 log.info("Starting bot...");
 
 // Forces deno to read all the files which will fill the commands/inhibitors cache etc.
-importDirectory(await Deno.realPath("./src/commands"));
-importDirectory(await Deno.realPath("./src/events"));
-
+await Promise.all(
+  [
+    "./src/commands",
+    "./src/events",
+    // "./src/tasks",
+  ].map((path) => importDirectory(Deno.realPathSync(path))),
+);
 await fileLoader();
 
 // UPDATES YOUR COMMANDS TO LATEST COMMANDS
